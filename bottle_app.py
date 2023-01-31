a7fee4a14811efb67fc9465f649281f110ca2945
adityagotlur0505 <39362928+adityagotlur0505@users.noreply.github.com>
2023-01-30 21:25:30 -0500

    Create bottle_app.py

---
+++ bottle_app.py
@@ -0,0 +1,23 @@
+# A very simple Bottle Hello World app for you to get started with...
+import os
+from bottle import default_app, route, run
+
+@route('/public')
+def hello_public():
+    return 'This is a  public message'
+@route('/secret')
+def hello_public():
+    return 'This is a  secret message'
+
+@route('/redirect')
+def hello_public():
+    return 'This is a  public message'
+    
+@route('/')
+def hello_world():
+    return 'Hello from Bottle!'
+
+if 'PYTHONANYWHERE_DOMAIN' in os.environ:
+    application = default_app()
+else:
+    run(host='localhost', port=8080)
